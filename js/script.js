9ea31ec6e57ce615f92249b24771e2cf2b6a676a
IgorSinyak <igorsinyak92@gmail.com>
2016-11-14 21:51:04 +0300

    fix_v1

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
+var BUTTON_SELECTOR = ".button";
+var BUTTON_SYMBOL_SELECTOR = ".fa"
+var CHECK_SYMBOL = "fa-check";
+var TIMES_SYMBOL = "fa-times";
+var WIDGET_N2_SELECTOR = "#widget_2";
+var SLIDE_BLOCK_SELECTOR = "#show_hide";
+var MENU_SELECTOR = ".selector"
+
 $(document).ready(function () {
-    //toggler
-    $(".white_box").click(function () {
-      var button = $(this).parent();
-      var check = button.children(".fa");
-      var widget = button.parent().parent();
-      if (button.hasClass("disabled")) {
-        button.removeClass("disabled").addClass("enabled");
-        check.removeClass("fa-times").addClass("fa-check");
-        if (widget.is("#widget_2")) {
-            $("#show_hide").slideDown();
-          }
-      } else { 
-          button.removeClass("enabled").addClass("disabled");
-          check.removeClass("fa-check").addClass("fa-times");
-          if (widget.is("#widget_2")) {
-            $("#show_hide").slideUp();
-          }
+  $(BUTTON_SELECTOR).click(function () {
+    var button = $(this);
+    var check = button.children(BUTTON_SYMBOL_SELECTOR);
+    var widget = button.parent().parent();
+    if (button.hasClass("disabled")) {
+      button.removeClass("disabled").addClass("enabled");
+      check.removeClass(TIMES_SYMBOL).addClass(CHECK_SYMBOL);
+      if (widget.is(WIDGET_N2_SELECTOR)) {
+        $(SLIDE_BLOCK_SELECTOR).slideDown();
+      }
+    } else { 
+      button.removeClass("enabled").addClass("disabled");
+      check.removeClass(CHECK_SYMBOL).addClass(TIMES_SYMBOL);
+        if (widget.is(WIDGET_N2_SELECTOR)) {
+          $(SLIDE_BLOCK_SELECTOR).slideUp();
+        }
       }
     });
-    //selector
-    //select_onClick
-    $(".selector").click(function (){
-      if ($(this).hasClass("active")){
-        $(this).removeClass("active");
-        $(this).children().css("display", "none");
-      } else {
-      $(".selector").children().css("display", "none");
-      $(".selector").removeClass("active");
+  $(MENU_SELECTOR).click(function (){
+    if ($(this).hasClass("active")){
+      $(this).removeClass("active");
+      $(this).children().css("display", "none");
+    } else {
+      $(MENU_SELECTOR).children().css("display", "none");
+      $(MENU_SELECTOR).removeClass("active");
       $(this).children().css("display", "block");
       $(this).addClass("active");
-      }
+    }
     });
-    //display_onHover
-    $(".selector").hover(function (){
+    $(MENU_SELECTOR).hover(function (){
       if (!$(this).hasClass("active")){
       $(this).children().css("display", "block");
     }},
